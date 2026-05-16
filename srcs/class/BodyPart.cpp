5419621620813e8953e3c3925a980158ace9c049
florian <florian.guiramand@protonmail.com>
2026-05-16 19:38:34 +0200

    feat: update BodyPart to use Vector4 for position, rotation, scale, and color

--- srcs/class/BodyPart.cpp
+++ srcs/class/BodyPart.cpp
@@ -2,18 +2,22 @@
 
 // CONSTRUCTOR - DESTRUCTOR ---------------------------------------------------
 BodyPart::BodyPart(const std::string &n): _name(n) {
-	this->_localPosition = {0, 0, 0};
-	this->_localRotation = {0, 0, 0};
-	this->_localScale    = {1, 1, 1};
+	this->_localPosition = Vector4(0.0, 0.0, 0.0);
+	this->_localRotation = Vector4(0.0, 0.0, 0.0);
+	this->_localScale    = Vector4(1.0, 1.0, 1.0);
+	this->_geometryOffset = Vector4(0.0, 0.0, 0.0);
+	this->_color = Vector4(0.8, 0.8, 0.8, 1.0);
 }
 
 BodyPart::~BodyPart() = default;
 
 
 // SETTER ---------------------------------------------------------------------
-void	BodyPart::setLocalPosition(const float x, const float y, const float z) { this->_localPosition = {x, y, z}; }
-void	BodyPart::setLocalRotation(const float x, const float y, const float z) { this->_localRotation = {x, y, z}; }
-void	BodyPart::setLocalScale(const float x, const float y, const float z) { this->_localScale = {x, y, z}; }
+void	BodyPart::setLocalPosition(const float x, const float y, const float z) { this->_localPosition = Vector4(x, y, z); }
+void	BodyPart::setLocalRotation(const float x, const float y, const float z) { this->_localRotation = Vector4(x, y, z); }
+void	BodyPart::setLocalScale(const float x, const float y, const float z) { this->_localScale = Vector4(x, y, z); }
+void	BodyPart::setGeometryOffset(const float x, const float y, const float z) { this->_geometryOffset = Vector4(x, y, z); }
+void	BodyPart::setColor(const float r, const float g, const float b) { this->_color = Vector4(r, g, b, 1.0); }
 
 BodyPart	*BodyPart::addChild(std::unique_ptr<BodyPart> child) {
 	child->_parent = this;
