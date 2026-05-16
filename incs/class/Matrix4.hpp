7e5efb70161677345fb7beb0d427549c0dacd1cf
florian <florian.guiramand@protonmail.com>
2026-05-16 19:39:57 +0200

    feat: update Matrix4 to use double for matrix operations

--- incs/class/Matrix4.hpp
+++ incs/class/Matrix4.hpp
@@ -12,16 +12,16 @@ class Matrix4 {
         double  matrix[4][4];
 
         Matrix4();
-        explicit Matrix4(int matrix[4][4]);
+        explicit Matrix4(const double matrix[4][4]);
         ~Matrix4();
         Matrix4(Matrix4 const& copy);
         Matrix4 &operator=(Matrix4 const& other);
 
         // create a null matrix with constructor then call the following functions
         void    identity();
-        void    translation(int x, int y, int z);
-        void    scaling(int x, int y, int z);
-        void    rotation(float angle, Axis axis);
+        void    translation(double x, double y, double z);
+        void    scaling(double x, double y, double z);
+        void    rotation(double angle, Axis axis);
 };
 
 #endif
